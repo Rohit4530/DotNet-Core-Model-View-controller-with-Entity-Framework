8b33a3a809599edde81b483f67e0c0cf3e301ddc
Rohit Gunjal <103052614+Rohit4530@users.noreply.github.com>
2023-11-07 17:26:41 +0530

    controller added

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,98 @@
+﻿using BulkyWeb.data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private ApplicationDBContext _dbContext;
+        public CategoryController(ApplicationDBContext db)
+        {
+            _dbContext = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _dbContext.Categories.ToList();
+            return View(objCategoryList);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder shouldn't match the Name");
+            }
+            if (ModelState.IsValid)
+            {
+                _dbContext.Categories.Add(obj);
+                _dbContext.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+            return View();
+
+        }
+        public IActionResult Edit(int id)
+        {
+            if ( id == 0)
+            {
+                return NotFound();
+            }
+            Category? category = _dbContext.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder shouldn't match the Name");
+            }
+            if (ModelState.IsValid)
+            {
+                _dbContext.Categories.Update(obj);
+                _dbContext.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+            return View();
+
+        }
+        public IActionResult Delete(int id)
+        {
+            if (id == 0)
+            {
+                return NotFound();
+            }
+            Category? category = _dbContext.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+        [HttpPost,ActionName("Delete")]
+        public IActionResult DeletePost(int? id)
+        {
+            Category? obj = _dbContext.Categories.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+                _dbContext.Categories.Remove(obj);
+                _dbContext.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+    }
+
+}
